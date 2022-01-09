9248cc923bdabfcfe606aa983268bce7f14fc889
Horuuuu <h2.globo@gmail.com>
2022-01-09 15:13:43 -0300

    grafico radar

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -70,6 +70,18 @@ function CardContainer() {
             Circular
           </NavLink>
         </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({
+              active: currentActiveTab === '4',
+            })}
+            onClick={() => {
+              toggle('4');
+            }}
+          >
+            Radar
+          </NavLink>
+        </NavItem>
       </Nav>
       {/*COMIENZO DE GRAFICO DE BARRAS */}
       <TabContent activeTab={currentActiveTab} className="border border-dark">
@@ -209,6 +221,72 @@ function CardContainer() {
               <div>
                 <div className="line">
                   <Pie
+                    data={{
+                      labels: [
+                        'Red',
+                        'Blue',
+                        'Yellow',
+                        'Green',
+                        'Purple',
+                        'Orange',
+                      ],
+                      datasets: [
+                        {
+                          label: '# of votes',
+                          data: [12, 19, 3, 5, 2, 3],
+                          backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                          ],
+                          borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                          ],
+                          borderWidth: 3,
+                          hoverBackgroundColor: 'white',
+                        },
+                      ],
+                    }}
+                    height={400}
+                    width={636}
+                    options={{
+                      maintainAspectRatio: false,
+                      scales: {
+                        yAxes: [
+                          {
+                            ticks: {
+                              beginAtZero: true,
+                            },
+                          },
+                        ],
+                      },
+                      legend: {
+                        labels: {
+                          fontSize: 25,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </TabPane>
+        {/*COMIENZO DE GRAFICO  RADAR */}
+        <TabPane tabId="4">
+          <Row>
+            <Col sm="12">
+              <div>
+                <div className="line">
+                  <Radar
                     data={{
                       labels: [
                         'Red',
