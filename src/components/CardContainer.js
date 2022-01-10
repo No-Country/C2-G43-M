8ee9f2b0f7fdb2df1fc32dd2c50428456a6bbf1f
Horuuuu <h2.globo@gmail.com>
2022-01-10 16:12:34 -0300

    cambio de colores neon

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+//import { Chart } from 'chart.js';
 import { Radar, Bar, defaults, Line, Pie } from 'react-chartjs-2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -11,7 +12,7 @@ import {
   Col,
 } from 'reactstrap';
 import classnames from 'classnames';
-defaults.global.legend.position = 'bottom'; //para que la decripcion este abajo
+defaults.global.legend.position = 'top'; //para que la decripcion este arriba,sino bottom
 
 function CardContainer() {
   // State for current active Tab
@@ -21,14 +22,27 @@ function CardContainer() {
   const toggle = (tab) => {
     if (currentActiveTab !== tab) setCurrentActiveTab(tab);
   };
+  //funciones par primer grafixo
+  let chartExample1 = {
+    data1: (canvas) => {
+      let ctx = canvas.getContext('2d');
+      //funciones gradient
+      const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+      gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+      gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+      gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+    },
+  };
 
   return (
-    <div
+    <div //ESTILOS DEL CONTENEDOR DE LOS GRAFICOS
       className="border border-dark"
       style={{
         display: 'block',
         width: 700,
         padding: 30,
+        background: '', //para caMbiar el color
       }}
     >
       <h4>Graficos</h4>
@@ -89,6 +103,7 @@ function CardContainer() {
           <Row>
             <Col sm="12">
               <div>
+                {/*BLOQUE DE CONFIGURACION */}
                 <div>
                   <Bar
                     data={{
@@ -102,32 +117,49 @@ function CardContainer() {
                       ],
                       datasets: [
                         {
-                          label: '# of votes',
-                          data: [12, 19, 3, 5, 2, 3],
+                          label: 'Número de ventas',
+                          fill: true /*
+                          //backgroundColor: 'gradientStroke',
+                          borderColor: '#1f8ef1',
+                          borderWidth: 2,
+                          borderDash: [],
+                          borderDashOffset: 0.0,
+                          pointBackgroundColor: '#1f8ef1',
+                          pointBorderColor: 'rgba(255,255,255,0)',
+                          pointHoverBackgroundColor: '#1f8ef1',
+                          pointBorderWidth: 20,
+                          pointHoverRadius: 4,
+                          pointHoverBorderWidth: 15,
+                          pointRadius: 4,*/,
+
+                          data: [
+                            100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100,
+                          ],
                           backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
+                            'rgb(255, 23, 0)',
+                            'rgb(4, 0, 154)',
+                            'rgb(251, 255, 0)',
+                            'rgb(6, 255, 0)',
+                            'rgb(154, 6, 128)',
+                            'rgb(255, 142, 0)',
                           ],
-                          borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                          ],
-                          borderWidth: 3,
-                          hoverBackgroundColor: 'white',
                         },
                       ],
                     }}
                     height={400}
                     width={600}
                     options={{
+                      //animacion
+                      animations: {
+                        backgroundColor: {
+                          type: 'color',
+                          duration: 2000,
+                          easing: 'linear',
+                          to: 'blue',
+                          from: 'red',
+                          loop: true,
+                        },
+                      },
                       maintainAspectRatio: false,
                       scales: {
                         yAxes: [
@@ -167,7 +199,7 @@ function CardContainer() {
                     ],
                     datasets: [
                       {
-                        label: '# of votes',
+                        label: 'Número de ventas',
                         data: [12, 19, 3, 5, 2, 3],
                         backgroundColor: [
                           'rgba(255, 99, 132, 0.2)',
@@ -187,6 +219,19 @@ function CardContainer() {
                         ],
                         borderWidth: 3,
                         hoverBackgroundColor: 'white',
+                      },
+                      {
+                        //segunda linea
+                        label: 'porcentaje de ventas',
+                        data: [22, 29, 13, 55, 22, 13],
+                        backgroundColor: [
+                          'rgba(55, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(55, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(53, 102, 255, 0.2)',
+                          'rgba(55, 159, 64, 0.2)',
+                        ],
                       },
                     ],
                   }}
@@ -232,7 +277,7 @@ function CardContainer() {
                       ],
                       datasets: [
                         {
-                          label: '# of votes',
+                          label: 'Número de ventas',
                           data: [12, 19, 3, 5, 2, 3],
                           backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
@@ -298,7 +343,7 @@ function CardContainer() {
                       ],
                       datasets: [
                         {
-                          label: '# of votes',
+                          label: 'Número de ventas',
                           data: [12, 19, 3, 5, 2, 3],
                           backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
@@ -319,11 +364,43 @@ function CardContainer() {
                           borderWidth: 3,
                           hoverBackgroundColor: 'white',
                         },
+                        {
+                          //segunda linea
+                          label: 'porcentaje de ventas',
+                          data: [22, 29, 13, 55, 22, 13],
+                          backgroundColor: [
+                            'rgba(55, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(55, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(53, 102, 255, 0.2)',
+                            'rgba(55, 159, 64, 0.2)',
+                          ],
+                        },
                       ],
                     }}
                     height={400}
                     width={636}
                     options={{
+                      //animacion
+                      animations: {
+                        backgroundColor: {
+                          type: 'color',
+                          duration: 1000,
+                          easing: 'linear',
+                          to: 'red',
+                          from: 'blue',
+                          loop: true,
+                        },
+
+                        /* tension: {
+                          duration: 1000,
+                          easing: 'linear',
+                          from: 1,
+                          to: 0,
+                          loop: true,
+                        },*/
+                      },
                       maintainAspectRatio: false,
                       scales: {
                         yAxes: [
