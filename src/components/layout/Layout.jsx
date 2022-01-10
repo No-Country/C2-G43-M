221fcff47aff54b78d57e56ee3005ba091289fa9
AleP1 <63692378+AleP1@users.noreply.github.com>
2022-01-10 02:40:42 -0300

    TopBar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
+import TopNav from "../topnav/TopNav";
 import "./layout.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -9,13 +10,16 @@ import Customers from "../../pages/Customers";
 const Layout = ({ props }) => {
   return (
     <BrowserRouter>
-      <div className="layout__content">
+      <div className="layout">
         <Sidebar {...props} />
-        <div className="layout__content-main">
-          <Routes>
-            <Route path="/customers" element={<Customers />} />
-            <Route exact path="/" element={<Dashboard />} />
-          </Routes>
+        <div className="layout__content">
+          <TopNav />
+          <div className="layout__content-main">
+            <Routes>
+              <Route path="/customers" element={<Customers />} />
+              <Route exact path="/" element={<Dashboard />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </BrowserRouter>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,51 @@
-import React from "react";
-import "./sidebar.css";
+import React from 'react'
 
+import { Link } from 'react-router-dom'
 
-import logo from "../../assets/images/favicon.png";
+import './sidebar.css'
 
-import sidebar_items from "../../assets/JsonData/sidebar_routes.json";
-import {Link} from "react-router-dom";
+import logo from '../../assets/images/favicon.png'
+import SidebarItem from './SidebarItem'
+import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
 
-const Sidebar = () => {
-  return <div className="sidebar">
-        <div className="sidebar__logo">
-            <img src={logo} alt="dashboard logo" />
+/*const SidebarItem = props => {
+
+    const active = props.active ? 'active' : ''
+
+    return (
+        <div className="sidebar__item">
+            <div className={`sidebar__item-inner ${active}`}>
+                <i className={props.icon}></i>
+                <span>
+                    {props.title}
+                </span>
+            </div>
         </div>
-        {
-            sidebar_items.map((item, index) => (
-                <Link to={item.route} key={index}>
-                    <div>
-                        {item.display_name}
-                    </div>
-                </Link>
-            ))
-        }
-        </div>;
-};
+    )
+}*/
 
-export default Sidebar;
+const Sidebar = props => {
+
+    const activeItem = sidebar_items.findIndex(item => item.route === window.location.pathname)
+
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__logo">
+                <img src={logo} alt="company logo" />
+            </div>
+            {
+                sidebar_items.map((item, index) => (
+                    <Link to={item.route} key={index}>
+                        <SidebarItem
+                            title={item.display_name}
+                            icon={item.icon}
+                            active={index === activeItem}
+                        />
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Sidebar
